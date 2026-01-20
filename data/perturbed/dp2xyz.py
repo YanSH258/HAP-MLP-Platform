20dd2714c4456f65104e6407f8d9745483d25d72
Yan Shuanghao <2024222030105@stu.scu.edu.cn>
2026-01-20 15:15:12 +0800

    精简数据

--- data/perturbed/dp2xyz.py
+++
@@ -1,5 +0,0 @@
-import dpdata
-from ase.io import write
-data = dpdata.System("batch_init", fmt="deepmd/npy")
-perturb_atoms = data.to_ase_structure()
-write("perturb.exyz", perturb_atoms, format='extxyz')
